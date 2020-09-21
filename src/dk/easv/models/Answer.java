72b9d4533fb0659871c5f1d9ece06b538a99d12c
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2020-09-21 16:44:57 +0200

    Added getters and setters for the answer class

--- src/dk/easv/models/Answer.java
+++ src/dk/easv/models/Answer.java
@@ -3,4 +3,20 @@ package dk.easv.models;
 public class Answer {
     private String value;
     private boolean isCorrect;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
 }
